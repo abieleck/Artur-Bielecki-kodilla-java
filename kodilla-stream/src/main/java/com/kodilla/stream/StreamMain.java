d6e197ea5312ec870304b909fa3969df34a9c7ba
“abieleck <abieleck@poczta.onet.pl>
2017-11-16 23:58:51 +0100

    7.1 Zadanie: Upiększacz tekstów

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,42 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Beautifying text");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Text to beautify", s -> "__" + s + "__");
+        poemBeautifier.beautify("Text to beautify", s -> s.toUpperCase());
+        poemBeautifier.beautify("Text to beautify", s -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i=0; i<s.length(); i++) {
+                stringBuilder.append(s.charAt(i));
+                stringBuilder.append('_');
+            }
+            if(s.length()>0) {
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            }
+            return stringBuilder.toString();
+        });
+        poemBeautifier.beautify("Text to beautify", s -> s.replace(' ', '_'));
+        poemBeautifier.beautify("Text to beautify", s -> (new StringBuilder(s)).reverse().toString());
+    }
+}
