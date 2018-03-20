8fbc8f37efd63ff4a061ac8318a93edef82098ac
“abieleck <abieleck@poczta.onet.pl>
2018-03-20 22:46:07 +0100

    Zadanie 23.3

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -0,0 +1,40 @@
+package com.kodilla.testing2.crudapp;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CrudAppTestingApp {
+    public static final String XPATH_INPUT =
+            "//html/body/app-root/mat-card/mat-card-content/mat-form-field/div/div/div/input";
+    public static final String XPATH_TEXTAREA =
+            "//html/body/app-root/mat-card/mat-card-content/mat-form-field/div/div/div/textarea";
+    public static final String XPATH_SELECT =
+            "//div[contains(@class,\"task-row\")]/mat-grid-tile/figure/mat-select";
+    //"//html/body/app-root/mat-card/mat-card-content/mat-grid-list/div/div/mat-grid-tile/figure/mat-select";
+    public static final String XPATH_SELECTION =
+            "//mat-option[2]";
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://abieleck.github.io/");
+
+        WebElement inputField = driver.findElement(By.xpath(XPATH_INPUT));
+        inputField.sendKeys("New robotic task");
+
+        WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
+        textareaField.sendKeys("The robotic content");
+
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+
+        WebElement selectCombo = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_SELECT)));
+        selectCombo.click();
+
+        WebElement selection = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_SELECTION)));
+        selection.click();
+        /*Select selectBoard = new Select(selectCombo);
+        selectBoard.selectByIndex(1);*/
+    }
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,39 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.LocalDate;
+
+public class FacebookTestingApp {
+    public static final LocalDate birthDate = LocalDate.of(1975, 8, 20);
+    public static final String XPATH_SELECT_DAY =
+            "//span[@data-name=\"birthday_wrapper\"]/span/select[@id=\"day\"]";
+    public static final String XPATH_SELECT_MONTH =
+            "//span[@data-name=\"birthday_wrapper\"]/span/select[@id=\"month\"]";
+    public static final String XPATH_SELECT_YEAR =
+            "//span[@data-name=\"birthday_wrapper\"]/span/select[@id=\"year\"]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        WebElement selectDayElement = driver.findElement(By.xpath(XPATH_SELECT_DAY));
+        Select selectDay = new Select(selectDayElement);
+        selectDay.selectByValue(String.valueOf(birthDate.getDayOfMonth()));
+
+        WebElement selectMonthElement = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
+        Select selectMonth = new Select(selectMonthElement);
+        selectMonth.selectByValue(String.valueOf(birthDate.getMonthValue()));
+
+        WebElement selectYearElement = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
+        Select selectYear = new Select(selectYearElement);
+        selectYear.selectByValue(String.valueOf(birthDate.getYear()));
+
+    }
+}
